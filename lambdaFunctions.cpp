ccc55f32735495095ce01379fe7babc0498b84f6
Vasu Bansal <vasubansal1998@gmail.com>
2021-05-15 17:58:10 +0530

    Upload-1

---
+++ lambdaFunctions.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    auto myFun = [](int x, int y){
+
+        return x+y;
+
+    };
+
+    cout<<myFun(5, 6)<<'\n';
+    cout<<myFun(123, 463);
+
+
+}
